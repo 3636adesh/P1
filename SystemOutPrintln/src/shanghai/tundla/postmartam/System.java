a9dba31c4ab0c11371adc85c7284d2221c993be7
Shanghaai <34318533+Shanghaai@users.noreply.github.com>
2017-12-13 12:23:06 +0530

    Add files via upload

---
+++ SystemOutPrintln/src/shanghai/tundla/postmartam/System.java
@@ -0,0 +1,15 @@
+package shanghai.tundla.postmartam;
+
+import java.io.PrintStream;
+
+public class System 
+{
+	static PrintStream out;
+	public static void main(String[] args) throws Exception
+    { 
+	   System.out.println("getting static variable ::::::::::::::::::::");
+	   System.out.println("SAHIL");
+    
+    }
+}
+
---
+++ SystemOutPrintln/src/shanghai/tundla/postmartam/Test.java
@@ -0,0 +1,12 @@
+package shanghai.tundla.postmartam;
+
+class Test extends System
+{
+	static String s = "SAHILshanghaai";
+	
+	public static void main(String[] args) 
+	{			
+		System.out.println(Test.s.length());
+		System.out.println(Test.s);
+	}
+}
